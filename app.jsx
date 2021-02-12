26c3ea418e5f45c900cddad93e87eeb658043dbd
kiani1 <dkiani456@gmail.com>
2021-02-12 20:05:42 +0500

    jsx testing

--- app.jsx
+++ app.jsx
@@ -1,48 +1,42 @@
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
-const myelement = <h1>I Love JSX!</h1>;
-const test2 = 1213123
-const test2= 123123123;
-const test2=;
-asdasdasdasdasdads
-ReactDOM.render(myelement, document.getElementById('root'));
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  
+  React.render(<HelloMessage name="John" />, mountNode);
