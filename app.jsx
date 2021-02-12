750dc05ac1abcd902f975fe2cddfb6eea754a51d
kiani1 <dkiani456@gmail.com>
2021-02-12 19:37:46 +0500

    jsx testing agian

--- app.jsx
+++ app.jsx
@@ -2,4 +2,5 @@ const myelement = <h1>I Love JSX!</h1>;
 const test2 = 1213123
 const test2= 123123123;
 const test2=;
+asdasdasdasdasdads
 ReactDOM.render(myelement, document.getElementById('root'));
