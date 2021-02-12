1757ef79bf4670aaf5402217d2ea4645cc84094b
kiani1 <dkiani456@gmail.com>
2021-02-12 19:56:25 +0500

    new lines added

--- app.jsx
+++ app.jsx
@@ -4,3 +4,45 @@ const test2= 123123123;
 const test2=;
 asdasdasdasdasdads
 ReactDOM.render(myelement, document.getElementById('root'));
+const myelement = <h1>I Love JSX!</h1>;
+const test2 = 1213123
+const test2= 123123123;
+const test2=;
+asdasdasdasdasdads
+ReactDOM.render(myelement, document.getElementById('root'));
+const myelement = <h1>I Love JSX!</h1>;
+const test2 = 1213123
+const test2= 123123123;
+const test2=;
+asdasdasdasdasdads
+ReactDOM.render(myelement, document.getElementById('root'));
+const myelement = <h1>I Love JSX!</h1>;
+const test2 = 1213123
+const test2= 123123123;
+const test2=;
+asdasdasdasdasdads
+ReactDOM.render(myelement, document.getElementById('root'));
+const myelement = <h1>I Love JSX!</h1>;
+const test2 = 1213123
+const test2= 123123123;
+const test2=;
+asdasdasdasdasdads
+ReactDOM.render(myelement, document.getElementById('root'));
+const myelement = <h1>I Love JSX!</h1>;
+const test2 = 1213123
+const test2= 123123123;
+const test2=;
+asdasdasdasdasdads
+ReactDOM.render(myelement, document.getElementById('root'));
+const myelement = <h1>I Love JSX!</h1>;
+const test2 = 1213123
+const test2= 123123123;
+const test2=;
+asdasdasdasdasdads
+ReactDOM.render(myelement, document.getElementById('root'));
+const myelement = <h1>I Love JSX!</h1>;
+const test2 = 1213123
+const test2= 123123123;
+const test2=;
+asdasdasdasdasdads
+ReactDOM.render(myelement, document.getElementById('root'));
