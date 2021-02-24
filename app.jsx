d46a3ccf17f2b78c53447f67139bbf93e33eb7f7
kiani1 <dkiani456@gmail.com>
2021-02-24 18:30:36 +0500

    testing

--- app.jsx
+++ app.jsx
@@ -28,6 +28,36 @@ var HelloMessage = React.createClass({
       return <div>Hello {this.props.name}</div>;
     }
   });
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
+  React.render(<HelloMessage name="John" />, mountNode);
+var HelloMessage = React.createClass({
+    render: function() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  });
   
   React.render(<HelloMessage name="John" />, mountNode);
 var HelloMessage = React.createClass({
